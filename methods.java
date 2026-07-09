9c21cd9a30c5761751109dbc216b07bb7f01bf31
gaurvee-tripathi <tripathigaurvee09@gmail.com>
2026-07-09 22:15:26 +0530

    update

---
+++ methods.java
@@ -0,0 +1,17 @@
+public class methods {
+    public static void sum(int a,int b){
+        //process
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+//        int sum =sum(8,9);
+//        System.out.println(sum);
+//
+//        double ans=Math.pow(sum);
+                methods.sum(8,9);
+
+
+    }
+}
